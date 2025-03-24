fe6a8bc67ef84b262498eee5d5ef2f895844a2ec
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2025-03-24 19:06:16 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -159,7 +159,7 @@ class _SecondScreenState extends State<SecondScreen> {
     String firstPart = parts.isNotEmpty ? parts.first.trim() : "";
     String port = portMapping[firstPart] ?? "7714"; 
     setState(() {
-      streamUrl = "https://portal.busads.in/proxy/?port=$port";
+      streamUrl = "https://luan.xyz/files/audio/nasa_on_a_mission.mp3";
     });
   }
 
